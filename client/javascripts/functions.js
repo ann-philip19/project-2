73924215d919ebb8268fac0f6f13961fc4c6ccde
Ann Philip <aphilip@uoregon.edu>
2020-04-27 23:57:41 -0700

    Functions

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,5 +1,42 @@
 // jshint esversion: 6
 
+//write a reverseOneWord function
+function reverseWord(word) {
+
+  return word;
+
+}
+
+let revWord1 = (sentence) => {
+  let wordArray = sentence.split(),
+    result ="";
+
+  for (let i = 0; i < wordArray.length; i++) {
+    let currentWord = wordArray[i];
+    let reversedWord = reverseWord(currentWord);
+  }
+
+  return result;
+};
+
+let revWord2 = (sent) => {
+  let arr = sent.split("");
+  let result = "";
+  arr.forEach((char)=>{
+    result = char + result;
+  })
+  return result;
+};
+
+let revWord3 = (sent) => {
+  let arr = sent.split("");
+  let result = "";
+  for (char of arr){
+    result = char + result;
+  }
+  return result;
+};
+
 let containsDuplicates = s => {
   let cArr = [0, 0, 0, 0],
     code;
